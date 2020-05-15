5254534304bb80b8f21bda580cb9c4bc6c209d0c
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-05-16 00:34:15 +0900

    Create Customer js

--- management/src/App.js
+++ management/src/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer'
+const customers = [
+  {
+    'id':1,
+    'image': 'https://placeimg.com/128/128/1',
+    'name':'홍길동',
+    'birthday':'0918',
+    'gender':'남',
+    'job':'의사',
+  },
+  {
+    'id':2,
+    'image': 'https://placeimg.com/128/128/2',
+    'name':'다다다',
+    'birthday':'0218',
+    'gender':'2남',
+    'job':'의사2',
+  },
+  {
+    'id':3,
+    'image': 'https://placeimg.com/128/128/3',
+    'name':'이이이',
+    'birthday':'0214',
+    'gender':'남3',
+    'job':'의사3',
+  }
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} let="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+]
+class App extends Component {
+  render(){ 
+    return (
+      <div>
+        {
+          customers.map(c =>{
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}/>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
